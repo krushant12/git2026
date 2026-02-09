3744c421adc5feb89da176532b4d32123c295252
krushant12 <149995819+krushant12@users.noreply.github.com>
2026-02-09 18:53:10 +0530

    if else learning

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-let firstCard = 3;
-let secondCard = 10;
+// let age = 12
 
-let sum = firstCard + secondCard
-console.log(sum);
+// if (age <= 21)
+// {
+//     console.log("You can not enter the club!");
+// }
+// else{
+//     console.log("Welcome");
+// }
+
+
+
+
+// let age = 101
+
+// if(age < 100)
+// {
+//     console.log("Not Elegible");
+// }
+// else if(age === 100)
+// {
+//     console.log("Here is your birthday card from the King!");
+// }
+// else
+// {
+//     console.log("Not elegible, you have already gotten one");
+// }
+
+
+
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+
+if (sum <= 20)
+{
+    console.log("Do you want to draw a new card? 🙂");
+}
+else if(sum === 21)
+{
+    console.log("Wohoo! You've got Blackjack! 🥳");
+}
+else
+{
+    console.log("You're out of the game! 😭")
+}
