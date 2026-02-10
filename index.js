d270e14d20fc6d1435d16150e2417d7958c4b1e2
krushant12 <149995819+krushant12@users.noreply.github.com>
2026-02-10 18:00:17 +0530

    in js file add some function and add some button in html file and add some css add

--- index.js
+++ index.js
@@ -1,53 +1,54 @@
-// let age = 12
-
-// if (age <= 21)
-// {
-//     console.log("You can not enter the club!");
-// }
-// else{
-//     console.log("Welcome");
-// }
-
-
-
-
-// let age = 101
-
-// if(age < 100)
-// {
-//     console.log("Not Elegible");
-// }
-// else if(age === 100)
-// {
-//     console.log("Here is your birthday card from the King!");
-// }
-// else
-// {
-//     console.log("Not elegible, you have already gotten one");
-// }
-
-
-
 let firstCard = 10;
 let secondCard = 11;
+let cards = [firstCard, secondCard]  // array - ordered list of items
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
+let message = "";
 
-if (sum <= 20)
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+//let sumEl = document.querySelector("#sum-el");
+let cardEl = document.getElementById("card-el");
+
+function startGame()
 {
-    console.log("Do you want to draw a new card? 🙂");
-}
-else if(sum === 21)
-{
-    console.log("Wohoo! You've got Blackjack! 🥳");
-    hasBlackJack = true;
-}
-else
-{
-    console.log("You're out of the game! 😭")
-    isAlive = false;
+    renderGame();
 }
 
-//Case Out!
-console.log(isAlive);
+function renderGame()
+{
+    // render ot firstcard and secondcard
+    cardEl.textContent = "Card: " + cards[0] + " " + cards[1];
+    // render out All the cards we have
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20)
+    {
+        message = "Do you want to draw a new card?";
+    }
+    else if(sum === 21)
+    {
+        message = "Wohoo! You've got Blackjack!";
+    }
+    else  
+    {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+
+    //Display the message in the messageEl using messageEl.textContent
+    messageEl.textContent = message;
+}
+
+// Create a function newCard() that logs out "Drawing a new card from the deck!"
+function newCard()
+{
+    console.log("Drawing a new card from the deck!");
+    let card = 7;
+    sum += card;
+
+    //push the card to the cards array
+    cards.push(card);
+    console.log(cards);
+    renderGame();
+}
