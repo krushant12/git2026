9def6e0ee09a364474efc06063fe048d44de4baa
krushant12 <149995819+krushant12@users.noreply.github.com>
2026-02-10 18:40:43 +0530

    create a loop file for the loop practice file name is "loops.js"

---
+++ loops.js
@@ -0,0 +1,44 @@
+ // count to ten
+
+ // we need to specify....
+
+ //where should we Start counting?
+ //where is the FINISH line?
+ //what is the STEP SIZE we should use?
+
+//  for (let count = 10; count < 21; count += 1)
+//  {
+//     console.log(count);
+
+//  }
+
+//  for (let counter = 0; counter <= 100; counter += 10)
+//  {
+//     console.log(counter);
+//  }
+
+// let messages = [
+//     "Hey, how's it going?",        
+//     "I'm great, thank you! How about you?",
+//     "All good. Been working on my portfolio lately.",
+//     "Same here!"
+// ]
+
+// DRY - Don't repeat yourself
+// console.log(messages[0])
+// console.log(messages[1])
+// console.log(messages[2])
+// console.log(messages[3])
+
+// for (let i = 0; i <= 3; i += 1)
+// {
+//     console.log(messages[i]);
+// }
+
+
+// let cards = [7,3,5]
+// for (let i = 0; i < cards.length; i++)
+// {
+//     console.log(cards[i])
+// }
+
