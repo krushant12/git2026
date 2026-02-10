7356d0734ee5275fae0a96965134e6a898e62ff2
krushant12 <149995819+krushant12@users.noreply.github.com>
2026-02-10 17:21:10 +0530

    Chnage a file name "Practic.js" add some array consepts and add push and pop mathod

---
+++ practice.js
@@ -0,0 +1,97 @@
+//booleans datatype
+
+// let hasDiscountCode = true;
+
+// function processOrder()
+// {
+//     if(hasDiscountCode)
+//     {
+//         console.log('Discount applied to food order');
+//     }
+//     else
+//     {
+//         console.log('No discount applied');
+//     }
+// }
+// processOrder()
+
+
+// let hasDiscountOffer = true;
+
+// function orderProcess()
+// {
+//     if(hasDiscountOffer)
+//     {
+//         console.log('Discount applied to food order');
+//         hasDiscountOffer = false;
+//     }
+//     else
+//     {
+//         console.log('No Discount applied');
+//     }
+// }
+// orderProcess()
+// orderProcess()
+
+
+// let age = 12
+
+// if (age <= 21)
+// {
+//     console.log("You can not enter the club!");
+// }
+// else{
+//     console.log("Welcome");
+// }
+
+
+
+
+// let age = 101
+
+// if(age < 100)
+// {
+//     console.log("Not Elegible");
+// }
+// else if(age === 100)
+// {
+//     console.log("Here is your birthday card from the King!");
+// }
+// else
+// {
+//     console.log("Not elegible, you have already gotten one");
+// }
+
+// array - order list of items
+// let featuredPosts = [
+//     "Check out my Netflix Clone", 
+//     "Here's the code for my Project", 
+//     "I've just relaunched my protfolio"
+// ]
+
+// console.log(featuredPosts[0]); //output= Check out my Netflix Clone
+
+// console.log(featuredPosts.length) //output = 3
+
+//array with an multiple datatypes
+let per = ["Krushant Bamroliya", 35, true]
+console.log(per[2]); //output= true
+
+//Array .push() and .pop()
+let cards = [7, 4]
+cards.push(6); //add a value in the last of array
+console.log(cards)
+
+cards.pop() //remove last value in the array
+console.log(cards)
+
+let newMessage = [
+    "Check out my Netflix Clone", 
+    "Here's the code for my Project", 
+    "I've just relaunched my protfolio"
+]
+newMessage.push("Same here!");
+console.log(newMessage);
+
+newMessage.pop()
+console.log(newMessage);
